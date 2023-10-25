6893052eec350257a0b6bfb4406e2a3440b06a95
Viktar <viktar.kudasau@gmail.com>
2023-10-25 10:21:10 +0300

    Update printingArray method (add array's elements from console)

--- src/PrintingArray.java
+++ src/PrintingArray.java
@@ -5,15 +5,16 @@ public class PrintingArray {
     static Scanner scanner = new Scanner(System.in);
     static void printArray() {
         System.out.println("Введите желаемый размер массива (кол-во элементов в массиве:)");
-        int sizeArray = scanner.nextInt();                    //Задаем размер массива целых чисел
-        int [] array = new int[sizeArray];                       //Задаем массив целых чисел
+        int sizeArray = scanner.nextInt();
+        double [] array = new double[sizeArray];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) Math.round((Math.random() * 201) - 100);    //Заполняем массив случайными числами
+            System.out.println("Введите число (элемент массива №" + i +"):");
+            array[i] = scanner.nextDouble();
         }
         System.out.println("Элементы массива, кратные 3:");
-        for (int i : array) {
-            if (i % 3 == 0 && i != 0) {   // Проверяем, делится ли эл-т массива на число 3 без остатка
-                System.out.print(" " + i);     // Выводим на печать эл-ты массива, кратные 3
+        for (double i : array) {
+            if (i % 3 == 0 && i != 0) {
+                System.out.print(" " + i);
             }
         }
     }
